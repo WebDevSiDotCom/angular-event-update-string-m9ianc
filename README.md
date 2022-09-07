2ccc12fb4859f0f6913463228d1f5c3823965105
WebDevSiDotCom <96456502+WebDevSiDotCom@users.noreply.github.com>
2022-09-07 16:25:00 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-event-update-string-m9ianc
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-event-update-string-m9ianc)
---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+
+export class AppComponent  {
+
+  constructor () {}
+
+  phrase = "It's going";
+
+  update() {
+    this.phrase+=" ..and going";
+  }
+  
+}
